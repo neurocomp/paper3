2506bd561c3352d731d93a936b8624b11e25060f
Rasoul Azizi <35546673+neurocomp@users.noreply.github.com>
2019-04-15 23:30:04 +0430

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+# ngrok code
+./ngrok authtoken 83XZ1ySFWageKGJ5LWJUt_ZDeaTc4ZAmp1CcFWPQKd
+# dependencies
+  {
+    "react": "^16.8.5",
+    "react-dom": "^16.8.5",
+    "react-scripts": "2.1.8"
+  }
+  
+  # git codes
+  
+  
+  git config --global user.email ”rasoul.azizi@gmail.com”
+  git config --global user.name ”Rasoul Azizi”
+  
+  git clone https://ineuron:bavaria2000@gitlab.com/ineuron/topnews.git
+  
+  
+  git add .
+  git commit -m "new commit"
+  git push origin master
+  
+  
+  
+ # git merge
+ git merge --abort
+ 
+# add remote project
+git remote set-url origin https://gitlab.com/ineuron/topnews.git
