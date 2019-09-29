872df439d1d32898dfd836ab75d895b222aa86b1
Rasoul Azizi <35546673+neurocomp@users.noreply.github.com>
2019-09-30 00:50:10 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,14 @@
   git config --global user.email ”rasoul.azizi@gmail.com”
   git config --global user.name ”Rasoul Azizi”
   
-  git clone https://ineuron:bavaria2000@gitlab.com/ineuron/topnews.git
-  git clone https://neurocomp:Elpasocounty$1979@github.com/neurocomp/paper3.git
+  #git clone https://ineuron:bavaria2000@gitlab.com/ineuron/topnews.git
+  git clone https://github.com/neurocomp/paper3.git
   
   git add .
   git commit -m "new commit"
   git push origin master
+  
+  (( Username: neurocomp))
   
   
   
